d96cc56e4ca7512cbd2a2c1816e0d5b619999b9f
AlexandraKrause <you@example.com>
2023-09-05 12:14:27 +0200

    05.09

---
+++ test.R
@@ -0,0 +1,17 @@
+install.packages('rsconnect')
+library(rsconnect)
+
+rsconnect::setAccountInfo(name='femiaculture',
+                          token='3CB74ABC31D8AF6832C12E7146993E25',
+                          secret='9Uu4PGkN9jNpcootZO8ZUVkEKmBTR1yJjGrytqsO')
+
+
+install.packages("shiny")
+install.packages("shinydashboard")
+install.packages("readr")
+install.packages("decisionSupport")
+install.packages("DiagrammeR")
+install.packages("tidyverse")
+install.packages("ggplot2")
+install.packages("plyr")
+install.packages("dplyr")
